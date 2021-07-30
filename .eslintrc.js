036ab63c668245c027eeb93b1e447542ce6775bc
Mikołaj <53621297+MikiWojak@users.noreply.github.com>
2021-07-30 20:47:46 +0200

    Prepare the app (#1)
    - Include eslint and prettier config files
    - Delete unnecessary files

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:vue/recommended'],
+    rules: {
+        'no-unused-vars': [
+            'error',
+            { vars: 'all', args: 'after-used', ignoreRestSiblings: true }
+        ],
+        'vue/html-indent': ['error', 4, { baseIndent: 1 }],
+        'vue/script-indent': ['error', 4, { baseIndent: 0 }],
+        'vue/max-attributes-per-line': ['off'],
+        'vue/html-self-closing': ['off']
+    },
+    parserOptions: {
+        sourceType: 'module'
+    },
+    overrides: [
+        {
+            files: ['src/**/*'],
+            parserOptions: {
+                parser: 'babel-eslint',
+                sourceType: 'module'
+            },
+            env: {
+                browser: true
+            }
+        }
+    ],
+    globals: {
+        $: true,
+        require: true,
+        process: true,
+        module: true
+    }
+};
