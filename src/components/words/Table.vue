359458b8cce32b55f472044263efde94b64ec490
MikiWojak <mikizarowa8@gmail.com>
2021-07-31 20:49:41 +0200

    Table for words - component

---
+++ src/components/words/Table.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>Table</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Table'
+};
+</script>
--- src/views/Words.vue
+++ src/views/Words.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
         <h1>Words</h1>
+
+        <words-table />
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Words'
+    name: 'Words',
+
+    components: {
+        WordsTable: () => import('@/components/words/Table')
+    }
 };
 </script>
